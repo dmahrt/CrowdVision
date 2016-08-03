ab87c13f97cafda25c96d1271f7aaf0b0655f32b
drew-mahrt <drew.mahrt@generalassemb.ly>
2016-08-03 16:36:28 -0400

    fixing firebase config problems

--- CrowdVision/app/src/main/java/net/dividedattention/crowdvision/Constants.java
+++ CrowdVision/app/src/main/java/net/dividedattention/crowdvision/Constants.java
@@ -4,6 +4,6 @@ package net.dividedattention.crowdvision;
  * Created by drewmahrt on 5/11/16.
  */
 public class Constants {
-    public final static String FIREBASE_BASE_URL = "https://crowdvision.firebaseio.com/";
+    public final static String FIREBASE_BASE_URL = "https://crowdvision-d92f7.firebaseio.com/";
     public final static String FIREBASE_EVENTS = FIREBASE_BASE_URL + "/events";
 }
